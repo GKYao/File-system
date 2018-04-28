f7c72fa1da8cb470152797232d80ee71ea6aea0c
Rickster607 <Rickster607@gmail.com>
2018-04-28 15:07:28 -0400

    sfs

--- src/sfs.c
+++ src/sfs.c
@@ -305,9 +305,10 @@ int sfs_unlink(const char *path)
 int sfs_open(const char *path, struct fuse_file_info *fi)
 {
 	int retstat = 0;
-	fi->fh = open(path, fi->flags);
 	log_msg("\nsfs_open(path\"%s\", fi=0x%08x)\n",
 			path, fi);
+
+
 	return retstat;
 }
 
