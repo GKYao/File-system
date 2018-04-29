ea6a2d17e63b00371b56b3ac82e8c053d6d543a5
Rickster607 <Rickster607@gmail.com>
2018-04-28 23:13:14 -0400

    free in write

--- src/sfs.c
+++ src/sfs.c
@@ -513,6 +513,7 @@ int sfs_write(const char *path, const char *buf, size_t size, off_t offset,
 		write_buf += cur;
 	}
 	inode->blocks = total_blocks;
+	free(write_buf);
 	free(total_write);
 	return retstat;
 }
